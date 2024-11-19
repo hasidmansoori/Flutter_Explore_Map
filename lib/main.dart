70b4b158e8c04c7f38a6935ddb4830584a59a116
hasidmansoori <67381761+hasidmansoori@users.noreply.github.com>
2024-11-19 08:32:04 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ class ExploreMapApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Explore Map',
       theme: ThemeData.light(),
       darkTheme: ThemeData.dark(),
